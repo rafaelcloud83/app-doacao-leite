599ae3e11eca4fd61f0539acf92c29e5d7e590d9
rafaelcloud83 <rafaelcloud83@gmail.com>
2024-09-22 17:48:07 -0300

    feat: implementado lib do icone do apk gerado, alteração do nome do app e liberado conexão com internet no AndroidManifest

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,6 +1,6 @@
 class AppUrl {
   //ip oracle cloud
-  //static String baseURL = 'http://204.216.172.48:8080';
+  static String baseURL = 'http://204.216.172.48:8080';
   //ip local da maquina servidor
-  static String baseURL = 'http://192.168.0.25:8080';
+  //static String baseURL = 'http://192.168.0.25:8080';
 }
