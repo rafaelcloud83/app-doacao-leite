2ce599039660cf57231cb0ef97d677a00e9d1234
rafaelcloud83 <rafaelcloud83@gmail.com>
2025-04-21 19:36:18 -0300

    feat:atualizado ip do servidor

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,4 +1,5 @@
 class AppUrl {
   //ip local da maquina servidor API
-  static String baseURL = 'http://192.168.0.235:8080';
+  //static String baseURL = 'http://192.168.0.235:8080';
+  static String baseURL = 'http://doaleite.rcs.ip-ddns.com:8080';
 }
